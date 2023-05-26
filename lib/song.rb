02e7be24af8ac245a39104b60314ad86a74285e4
Patrick <muilipato@gmail.com>
2023-05-26 10:42:08 +0300

    completed lab

--- lib/song.rb
+++ lib/song.rb
@@ -3,16 +3,35 @@ class Song
 
  @@count = 0
  @@artists = []
+ @@genres = []
 
  def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
   @@count +=1
+  @@artists << self.artist
+  @@genres << self.genre
  end
 
  def self.count
   @@count
+ end
+
+ def self.artists
+  @@artists.uniq
+ end
+
+ def self.genres
+  @@genres.uniq
+ end
+
+ def self.genre_count
+  @@genres.tally
+
+ end
+ def self.artist_count
+  @@artists.tally
  end
 
 end
